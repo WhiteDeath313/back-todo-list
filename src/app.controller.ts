13531855faef58e25e21a321ac3a70948a620cc6
WhiteDeath313 <arnaud.ridet@epitech.eu>
2021-09-04 20:10:27 +0200

    feat(router): added tasks route + added fake data to test the front

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Task } from './Task';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,9 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('tasks')
+  getTasks(): Task[] {
+    return this.appService.getTasks();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './Task';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+  getTasks(): Task[] {
+    //Connection to bdd for getting all tasks
+    let test: Task = {
+      _id: '1',
+      _description: 'Simple CRUD in nest.js, mongodb and angular',
+      _isDone: true,
+      _title: 'Task to do'
+    };
+    return [test, test];
   }
 }
